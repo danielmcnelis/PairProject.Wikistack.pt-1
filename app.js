bcd178e45da3befe8d817865d978c849a8647b43
Daniel McNelis <dwm253@gmail.com>
2020-03-12 12:03:38 -0400

    init sequelize

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const morgan = require("morgan");
 const layout = require('./views/layout.js')
 const app = express();
+const models = require('./models');
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
@@ -14,14 +15,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
 // app.use('/posts', routes);
+models.db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.get("/", (req, res) => {
   let html = `<h1>HELLO WORLD</h1>`
   res.send(layout(html));
 })
 
-const PORT = 3030;
+const init = async () => {
+  await models.db.sync()
+  const PORT = 3030;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
+
+init()
